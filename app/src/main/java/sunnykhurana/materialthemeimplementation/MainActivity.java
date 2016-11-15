ca2fa1e1668775852cacf720dbf9d96315179ed0
sunnykhurana7 <sunnycreative7@gmail.com>
2016-11-15 16:30:30 +0530

    Initial commit

---
+++ app/src/main/java/sunnykhurana/materialthemeimplementation/MainActivity.java
@@ -0,0 +1,13 @@
+package sunnykhurana.materialthemeimplementation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
